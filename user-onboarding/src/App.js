bfd5b009372ccca3d492a417180dda5168d44f21
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2021-12-15 17:18:39 -0500

    Created Form component, imported to App.js, created the Users Form

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,9 +1,30 @@
+import React, { useState, useEffect } from "react";
+import Form from "./components/Form";
 import logo from "./logo.svg";
 import "./App.css";
 
+const initialFormValues = {
+  name: "",
+  email: "",
+  password: "",
+  terms: false,
+};
+
+const initialUsers = [];
+const initialDisabled = true;
+
 function App() {
+  const [users, setUsers] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
   return (
-    <div className="App">{/* <header className="App-header"></header> */}</div>
+    <div className="App">
+      <header>
+        <h1>Users App</h1>
+      </header>
+      <Form values={formValues} />
+    </div>
   );
 }
 
